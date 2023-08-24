03bd85568e6ef03b9c661b221c4010b084075000
queens kisivuli <queenskisivuli@gmail.com>
2023-08-24 23:21:45 +0300

    user currency converter

---
+++ sendMessage/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sendMessage.CurrencyConverter
+{
+    public class CurrencyConverterUsdToEur
+    {
+        // functions to convert USDToEUR
+        static public double USDToEUR(double USD)
+        {
+            return USD * 0.91;
+        }
+    }
+}
