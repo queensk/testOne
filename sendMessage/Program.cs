7d728d355105e577a69aee101eea2b9bc39ef139
queens kisivuli <queenskisivuli@gmail.com>
2023-08-24 23:22:54 +0300

    user banchmarks

---
+++ sendMessage/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using sendMessage.CurrencyConverter;
+using BenchmarkDotNet.Running;
+using sendMessage.test;
+
+class Program
+{
+    CurrencyConverterUsdToEur currencyConverter = new CurrencyConverterUsdToEur();
+
+    static void Main(string[] args)
+    {
+        Console.Title = "Email address validation performance testing";
+        var summary = BenchmarkRunner.Run<EmailValidatorBenchmark>();
+        Console.ReadLine();
+    }
+}
