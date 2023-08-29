a9b394197bf9eca8d48280b157c97e1b8043000c
queens kisivuli <queenskisivuli@gmail.com>
2023-08-29 12:29:26 +0300

    main class

--- sendMessage/test/performance.cs
+++ sendMessage/test/performance.cs
@@ -1,28 +1,28 @@
-using BenchmarkDotNet.Attributes;
-using sendMessage.Models;
+// using BenchmarkDotNet.Attributes;
+// using sendMessage.Models;
 
-namespace sendMessage.test
-{
-    [MemoryDiagnoser]
-    public class EmailValidatorBenchmark
-    {
-        private readonly string _sampleEmail;
+// namespace sendMessage.test
+// {
+//     [MemoryDiagnoser]
+//     public class EmailValidatorBenchmark
+//     {
+//         private readonly string _sampleEmail;
 
-        public EmailValidatorBenchmark()
-        {
-            _sampleEmail = "tony.stark@example.net";
-        }
+//         public EmailValidatorBenchmark()
+//         {
+//             _sampleEmail = "tony.stark@example.net";
+//         }
 
-        [Benchmark]
-        public bool TestIsValidEmail()
-        {
-            return User.IsValidEmail(_sampleEmail);
-        }
+//         [Benchmark]
+//         public bool TestIsValidEmail()
+//         {
+//             return User.IsValidEmail(_sampleEmail);
+//         }
 
-        [Benchmark]
-        public bool TestIsValidEmailRegex()
-        {
-            return User.IsValidEmailClass(_sampleEmail);
-        }
-    }
-}
+//         [Benchmark]
+//         public bool TestIsValidEmailRegex()
+//         {
+//             return User.IsValidEmailClass(_sampleEmail);
+//         }
+//     }
+// }
